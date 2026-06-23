8fa7dc8ca502aba52b2196307813c6a8c2b8db4f
mdmoheburrahmanmonir <mdmohiburrahmanmanik@gmail.com>
2026-06-23 13:22:57 +0600

    Env file
    MONGO_DB_URI=mongodb+srv://hireloop:sGKnk14AhKpxNdKW@cluster0.ifhtaoi.mongodb.net/?appName=Cluster0
    PORT=5000

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ app.use(cors())
 
 
 const uri = process.env.MONGO_DB_URI;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+ 
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
